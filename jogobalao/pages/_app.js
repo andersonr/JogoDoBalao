d0f541da34c046f5f96c3e09277650f2a6ab4a05
andersonr <anderson.rissardi@hotmail.com>
2021-05-21 00:19:45 -0300

    Adicionado o template gratuito do CreativeTim - Já em Next com Material

--- jogobalao/pages/_app.js
+++ jogobalao/pages/_app.js
@@ -1,31 +1,91 @@
-import ScopedCssBaseline from "@material-ui/core/ScopedCssBaseline";
-import { CssBaseline } from "@material-ui/core";
-import Head from "next/head";
-import { ThemeProvider } from "@material-ui/core/styles";
+/*!
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <ThemeProvider>
-      <CssBaseline />
+=========================================================
+* NextJS Material Kit v1.2.0 based on Material Kit Free - v2.0.2 (Bootstrap 4.0.0 Final Edition) and Material Kit React v1.8.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/nextjs-material-kit
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/nextjs-material-kit/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "next/app";
+import Head from "next/head";
+import Router from "next/router";
+
+import PageChange from "components/PageChange/PageChange.js";
+
+import "styles/scss/nextjs-material-kit.scss?v=1.2.0";
+
+Router.events.on("routeChangeStart", (url) => {
+  console.log(`Loading: ${url}`);
+  document.body.classList.add("body-page-transition");
+  ReactDOM.render(
+    <PageChange path={url} />,
+    document.getElementById("page-transition")
+  );
+});
+Router.events.on("routeChangeComplete", () => {
+  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
+  document.body.classList.remove("body-page-transition");
+});
+Router.events.on("routeChangeError", () => {
+  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
+  document.body.classList.remove("body-page-transition");
+});
+
+export default class MyApp extends App {
+  componentDidMount() {
+    let comment = document.createComment(`
+
+=========================================================
+* NextJS Material Kit v1.2.0 based on Material Kit Free - v2.0.2 (Bootstrap 4.0.0 Final Edition) and Material Kit React v1.8.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/nextjs-material-kit
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/nextjs-material-kit/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+`);
+    document.insertBefore(comment, document.documentElement);
+  }
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <React.Fragment>
         <Head>
-          <title>Jogo do Balão</title>
-          <meta
-            name="description"
-            content="Jogo do Balão mais zika do universo"
-          />
           <meta
             name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
-          <link rel="icon" href="/favicon.ico" />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
+          <title>NextJS Material Kit by Creative Tim</title>
         </Head>
-        <Component {...pageProps} />      
-    </ThemeProvider>
-  );
+        <Component {...pageProps} />
+      </React.Fragment>
+    );
+  }
 }
-
-export default MyApp;
--- jogobalao/pages/index.js
+++ jogobalao/pages/index.js
@@ -1,93 +1,12 @@
-// import "fontsource-roboto";
+import React, { Component } from "react";
+import Router from "next/router";
 
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import Button from "@material-ui/core/Button";
-import ScopedCssBaseline from "@material-ui/core/ScopedCssBaseline";
-import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+export default class Index extends Component {
+  componentDidMount = () => {
+    Router.push("/components");
+  };
 
-export default function Home() {
-  return (
-    <ScopedCssBaseline>
-      <div className={styles.container}>
-        {/* <Head>
-          <title>Jogo do Balão</title>
-          <meta
-            name="description"
-            content="Jogo do Balão mais zika do universo"
-          />
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width"
-          />
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-        </Head> */}
-
-        <main className={styles.main}>
-          <h1 className={styles.title}>
-            Welcome to <a href="https://nextjs.org">Next.js!</a>
-          </h1>
-
-          <Button variant="contained" color="primary">
-            <AccessAlarmIcon color="secondary"></AccessAlarmIcon>
-            Bad MotherFucker
-          </Button>
-
-          <p className={styles.description}>
-            Get started by editing{" "}
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-
-          <div className={styles.grid}>
-            <a href="https://nextjs.org/docs" className={styles.card}>
-              <h2>Documentation &rarr;</h2>
-              <p>Find in-depth information about Next.js features and API.</p>
-            </a>
-
-            <a href="https://nextjs.org/learn" className={styles.card}>
-              <h2>Learn &rarr;</h2>
-              <p>Learn about Next.js in an interactive course with quizzes!</p>
-            </a>
-
-            <a
-              href="https://github.com/vercel/next.js/tree/master/examples"
-              className={styles.card}
-            >
-              <h2>Examples &rarr;</h2>
-              <p>Discover and deploy boilerplate example Next.js projects.</p>
-            </a>
-
-            <a
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              className={styles.card}
-            >
-              <h2>Deploy &rarr;</h2>
-              <p>
-                Instantly deploy your Next.js site to a public URL with Vercel.
-              </p>
-            </a>
-          </div>
-        </main>
-
-        <footer className={styles.footer}>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{" "}
-            <span className={styles.logo}>
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                width={72}
-                height={16}
-              />
-            </span>
-          </a>
-        </footer>
-      </div>
-    </ScopedCssBaseline>
-  );
+  render() {
+    return <div />;
+  }
 }
